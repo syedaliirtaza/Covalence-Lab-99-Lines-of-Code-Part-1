faeee38ff9089c1e19f3f9fa0a50c709ca669139
syedaliirtaza <67573427+syedaliirtaza@users.noreply.github.com>
2020-06-29 11:16:02 +0500

    Add files via upload
    added html css and java script files

---
+++ lab4.js
@@ -0,0 +1,9 @@
+var friends = ['ali','will','matt','mutu','moon'];
+for(let i = 0;i<friends.length;i++)
+{
+    for(let j=99;j>0;j--)
+    {
+        console.log([j] + 'lines of code in the file' + [j] + "lines of code"  + (friends[i]) + ' strikes one out, clears it all out, ' + ([j] - 1) + ' lines of code in the file');
+    }
+    
+}
